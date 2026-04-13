cc8a849bcbad1e855ccc62efcbd519ef0ecf3912
PR-CI-CD <practicepipeline9393@gmail.com>
2026-04-13 12:09:04 +0100

    Fixed wrong import 3

--- apps/server/controllers/authController.js
+++ apps/server/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models/User');
+const User = require('../models/User');
 
 const createToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
